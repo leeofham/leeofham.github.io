7421211a189c8b07c314666d5df4f28744828934
Liam Roberts <liamroberts300@gmail.com>
2019-06-18 15:16:35 +0100

    added some JS to toggle the cards and added content to hidden card info and icons that link to project and github README

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log('JS loaded')
+
+const showMoreButton = document.querySelectorAll('.showMore')
+const cardInfo = document.querySelectorAll('.card-info')
+const cardBasic = document.querySelectorAll('.card-basic')
+
+
+showMoreButton.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    toggleCard(index)
+  })
+})
+
+function toggleCard(index){
+  if(cardBasic[index].style.display !== 'none'){
+    cardBasic[index].classList.toggle('hidden')
+    cardInfo[index].classList.toggle('hidden')
+    showMoreButton[index].classList.toggle('topButton')
+    showMoreButton[index].classList.toggle('fa-ellipsis-v')
+    showMoreButton[index].classList.toggle('fa-times-circle')
+  }else{
+    cardBasic[index].classList.toggle('hidden')
+    cardInfo[index].classList.toggle('hidden')
+    showMoreButton[index].classList.toggle('topButton')
+    showMoreButton[index].classList.toggle('fa-ellipsis-v')
+    showMoreButton[index].classList.toggle('fa-times-circle')
+  }
+}
